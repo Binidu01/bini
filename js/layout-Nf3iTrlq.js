44ef0a687dbf93dc4700e91def8bd1270c0ef221
Binidu Ranasinghe <161937364+Binidu01@users.noreply.github.com>
2026-03-13 22:11:38 +0530

    Delete js directory

--- js/layout-Nf3iTrlq.js
+++
@@ -1,1 +0,0 @@
-import{j as s,O as i}from"./index-I1Ahy8Wl.js";function n(){return s.jsx("div",{className:"plugins",children:s.jsx("main",{children:s.jsx(i,{})})})}export{n as default};
--- js/layout-YG9Y20pN.js
+++
@@ -1,1 +0,0 @@
-import{j as s,O as i}from"./index-I1Ahy8Wl.js";function n(){return s.jsx("div",{className:"config",children:s.jsx("main",{children:s.jsx(i,{})})})}export{n as default};
