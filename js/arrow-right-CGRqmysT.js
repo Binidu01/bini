997971e8925145aad6996bfdd01d7872de15afcd
Binidu Ranasinghe <161937364+Binidu01@users.noreply.github.com>
2026-09-10 13:28:20 +0530

    Add files via upload

---
+++ js/arrow-right-CGRqmysT.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./Layout-DQi0Unph.js";var t={name:`arrow-right`,size:24,node:[[`path`,{d:`M5 12h14`,key:`1ays0h`}],[`path`,{d:`m12 5 7 7-7 7`,key:`xquz4c`}]]};t.node;var n=e(t);export{n as t};
